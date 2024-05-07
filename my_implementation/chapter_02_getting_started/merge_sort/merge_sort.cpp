0b97e84f1d1df9cb80349b37e81ee1517d8cd23b
OccupyMars2025 <3119002196@qq.com>
2024-05-07 10:01:06 +0800

    add merge sort

---
+++ my_implementation/chapter_02_getting_started/merge_sort/merge_sort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include "vector_utils.hpp"
+
+
+void merge(std::vector<int>& left, std::vector<int>& right, std::vector<int>& vec){
+    std::size_t i = 0;
+    std::size_t j = 0;
+    std::size_t k = 0;
+
+    while (i < left.size() && j < right.size()) {
+        if(left[i] <= right[j]){
+            vec[k++] = left[i++];
+        }else{
+            vec[k++] = right[j++];
+        }
+    }
+
+    while (i < left.size()) {
+        vec[k++] = left[i++];
+    }
+
+    while (j < right.size()) {
+        vec[k++] = right[j++];
+    }
+}
+
+
+void mergeSort(std::vector<int>& vec){
+    if(vec.size() <= 1){
+        return;
+    }
+    std::vector<int> left(vec.begin(), vec.begin() + (vec.size()/2));
+    std::vector<int> right(vec.begin() + (vec.size()/2), vec.end());
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, vec);
+}
+
+
+int main(int argc, char const *argv[])
+{
+    for (int n = 1; n < 1000; ++n) {
+        std::vector<int> randomVector = generateRandomVector(n, -100, 100);
+        mergeSort(randomVector);
+        assert(isSorted(randomVector));
+    }
+    std::cout << "All tests passed!" << std::endl;
+
+    return 0;
+}
